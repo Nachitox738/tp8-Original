39e758f6a8ec559325b5a071d705add77fe8b653
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-26 05:14:30 -0300

    odiogithubpages

--- public/src/footer.js
+++ public/src/footer.js
@@ -7,12 +7,12 @@ const footer = `
                     <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
                         <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
                     </a>
-                    <span class="mb-3 mb-md-0 text-white">Ares Ignacio</span>
+                    <span class="mb-3 mb-md-0 text-primary">Ares Ignacio</span>
                 </div>
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-twitter text-white"></i></a></li>
-                    <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-facebook text-white"></i></a></li>
-                    <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-instagram text-white"></i></a></li>
+                    <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-twitter text-primary"></i></a></li>
+                    <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-facebook text-primary"></i></a></li>
+                    <li class="ms-3"><a class="text-body-secondary" href="#"><i class="bi bi-instagram text-primary"></i></a></li>
                 </ul>
             </footer>
         </div>
--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,4 @@
-import{navbar} from '/navbar.js';
+import{navbar} from 'navbar.js';
 document.querySelector('#contenedornav').innerHTML = navbar;
 let botonIngresar = document.querySelector('#botonIngresar');
 var usuarioDropdown = document.querySelector('#salir');
@@ -6,7 +6,7 @@ botonIngresar.addEventListener('click', ()=>{
     botonIngresar.style.display = "none";
     usuarioDropdown.style.display = "block";
 });
-import{footer} from '/footer.js';
+import{footer} from 'footer.js';
 document.querySelector('#contenedorfooter').innerHTML = footer;
 var botonLogOut = document.querySelector('#botonLogOut');
 botonLogOut.addEventListener('click', ()=>{
