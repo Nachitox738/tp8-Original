abf448f2c6e5b216950e2d8bf7bd2a1e69e97755
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-26 05:07:16 -0300

    noandanada3

--- public/src/index.js
+++ public/src/index.js
@@ -1,15 +1,23 @@
-import{navbar} from '/navbar.js';
-document.querySelector('#contenedornav').innerHTML = navbar;
-let botonIngresar = document.querySelector('#botonIngresar');
-var usuarioDropdown = document.querySelector('#salir');
-botonIngresar.addEventListener('click', ()=>{
-    botonIngresar.style.display = "none";
-    usuarioDropdown.style.display = "block";
-});
-import{footer} from '/footer.js';
-document.querySelector('#contenedorfooter').innerHTML = footer;
-var botonLogOut = document.querySelector('#botonLogOut');
-botonLogOut.addEventListener('click', ()=>{
-    botonIngresar.style.display = "block";
-    usuarioDropdown.style.display = "none";
+
+import { navbar } from './navbar.js';
+import { footer } from './footer.js';
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('#contenedornav').innerHTML = navbar;
+    document.querySelector('#contenedorfooter').innerHTML = footer;
+
+    let botonIngresar = document.querySelector('#botonIngresar');
+    let usuarioDropdown = document.querySelector('#salir');
+    let botonLogOut = document.querySelector('#botonLogOut');
+
+    botonIngresar.addEventListener('click', () => {
+        botonIngresar.style.display = "none";
+        usuarioDropdown.style.display = "block";
+        botonLogOut.style.display = "block"; 
+    });
+    botonLogOut.addEventListener('click', () => {
+        botonIngresar.style.display = "block";
+        usuarioDropdown.style.display = "none";
+        botonLogOut.style.display = "none";  
+    });
 });
