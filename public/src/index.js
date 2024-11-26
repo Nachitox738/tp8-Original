19c4316cf149976280aa79e2f66c35660da2a3cc
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-26 05:17:58 -0300

    odiogithubpages3

--- public/src/index.js
+++ public/src/index.js
@@ -1,4 +1,4 @@
-import{navbar} from 'public/src/navbar.js';
+import{navbar} from '/public/src/navbar.js';
 document.querySelector('#contenedornav').innerHTML = navbar;
 let botonIngresar = document.querySelector('#botonIngresar');
 var usuarioDropdown = document.querySelector('#salir');
@@ -6,7 +6,7 @@ botonIngresar.addEventListener('click', ()=>{
     botonIngresar.style.display = "none";
     usuarioDropdown.style.display = "block";
 });
-import{footer} from 'public/src/footer.js';
+import{footer} from '/public/src/footer.js';
 document.querySelector('#contenedorfooter').innerHTML = footer;
 var botonLogOut = document.querySelector('#botonLogOut');
 botonLogOut.addEventListener('click', ()=>{
