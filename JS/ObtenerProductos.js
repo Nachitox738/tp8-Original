5fc950debbe2fccd75c4a7e0973e044ee27089a7
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-27 00:22:23 -0300

    alold

--- JS/ObtenerProductos.js
+++ JS/ObtenerProductos.js
@@ -1,36 +1,32 @@
 fetch('./data/datos.json')
-    .then(respuesta => {
-        if (!respuesta.ok) {
-            throw new Error('Error al cargar el archivo JSON');
-        }
-        return respuesta.json();
-    })
-    .then(datos => {
-        console.log(datos);
-        mostrarProductos(datos.productos);
-    })
-    .catch(error => console.log('Error al cargar los productos:', error));
+    .then(respuesta => respuesta.json())
+    .then(datos => obtenerDatos(datos))
 
 let productos = ''
+const contenedor = document.querySelector('#divProdNuevo')
 
-const mostrarProductos = (productos) => {
- 
-    let contenido = '';
-    const contenedor = document.querySelector('#divProd');
-    
-    productos.forEach(producto => {
-        contenido += 
-        `<div class="card border border-1 border-dark d-flex flex-column align-items-center"
-            style="width: 100%; max-width: 300px; margin:30px">
-            <img src="${producto.imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h4>${producto.titulo}</h4>
-                <p class="card-text">${producto.descripcion}</p>
-            </div>
-            <p class="card-text border border-secondary rounded p-2"><strong>${producto.precio}</strong></p>
-            <button class="btn btn-outline-success mt-auto mb-3" type="submit">Comprar</button>
-        </div>`;
-    });
+const obtenerDatos = async () => {
+    try {
+        const respuesta = await fetch(endpoint)
+        productosRecibidos = await respuesta.json()
+        productosRecibidos.forEach(prod => {
+            productos +=
+                `<div class="card border border-1 border-dark d-flex flex-column align-items-center" style="width: 100%; max-width: 300px; margin:30px">
+                    <img src="${prod.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body ">
+                        <h4>${prod.titulo}</h4>
+                        <p class="card-text ">${prod.descripcion}</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center w-100 mb-2 px-2">
+                        <p class="card-text border border-secondary rounded p-2 mb-0">
+                          <strong>${prod.precio}</strong>
+                        </p>
+                    </div>      
+                </div>`
 
-    contenedor.innerHTML = contenido;
+        })
+        contenedor.innerHTML = productos
+    } catch (error) {
+        mostrarMensaje('error al cargar productos')
+    }
 }
