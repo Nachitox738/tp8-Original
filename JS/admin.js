4b668bb3e39a05909ec395e00c2f052b1d5afd2c
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-26 21:39:04 -0300

    FUNCIONAPLS

--- JS/admin.js
+++ JS/admin.js
@@ -27,27 +27,27 @@ const contenedor = document.querySelector('#divProdNuevo')
 
 const obtenerDatos = async () => {
   try {
-    const respuesta = await fetch(endpoint)
-    productosRecibidos = await respuesta.json()
-    productosRecibidos.forEach(prod => {
-      productos +=
+    let productos = ''
+    const contenedor = document.querySelector('#divProd')
+    datos.forEach(datos => {
+        productos += 
         `<div class="card border border-1 border-dark d-flex flex-column bg-dark text-light align-items-center"
                   style="width: 100%; max-width: 300px; margin:30px">
-                  <img src="${prod.imagen}" class="card-img-top" alt="...">
+                  <img src="${datos.imagen}" class="card-img-top" alt="...">
                   <div class="card-body ">
-                      <h4>${prod.titulo}</h4>
-                      <p class="card-text ">${prod.descripcion}</p>
+                      <h4>${datos.titulo}</h4>
+                      <p class="card-text ">${datos.descripcion}</p>
                   </div>
       <div class="d-flex justify-content-between align-items-center w-100 mb-2 px-2">
         <p class="card-text border border-secondary rounded p-2 mb-0">
-          <strong>${prod.precio}</strong>
+          <strong>${datos.precio}</strong>
         </p>
         <div class="d-flex ms-auto">
-          <a href="#prodEditar" class="btn btn-outline-warning me-2 edit" onClick="editar(${prod.id})">
+          <a href="#prodEditar" class="btn btn-outline-warning me-2 edit" onClick="editar(${datos.id})">
             <i class="bi bi-pencil"></i>
           </a>
           
-          <a class="btn btn-outline-danger" type="submit" id="eliminar" onClick="eliminar(${prod.id})">
+          <a class="btn btn-outline-danger" type="submit" id="eliminar" onClick="eliminar(${datos.id})">
             <i class="bi bi-trash" id="eliminar"></i>
           </a>
         </div>
