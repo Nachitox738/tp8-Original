ea87325f045d42a31648ddf4576192608c67c474
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-26 23:48:05 -0300

    sdddd

--- JS/ObtenerProductos.js
+++ JS/ObtenerProductos.js
@@ -1,38 +1,26 @@
 fetch('./data/datos.json')
-    .then(respuesta => {
-        if (!respuesta.ok) {
-            throw new Error('Error al cargar el archivo JSON');
-        }
-        return respuesta.json();
-    })
-    .then(datos => {
-        console.log(datos);
-        mostrarProductos(datos.productos);
-    })
-    .catch(error => console.log('Error al cargar los productos:', error));
 
-const mostrarProductos = (productos) => {
-    if (!Array.isArray(productos)) {
-        console.error('La variable "productos" no es un array:', productos);
-        return;
-    }
-    
-    let contenido = '';
-    const contenedor = document.querySelector('#divProd');
-    
-    productos.forEach(producto => {
-        contenido += 
+    .then(respuesta => respuesta.json())
+    // .then(datos=> console.log(datos))
+    .then(datos => mostrarProductos(datos))
+
+const mostrarProductos = (datos) => {
+    let productos = ''
+    const contenedor = document.querySelector('#divProd')
+    datos.forEach(datos => {
+        productos += 
         `<div class="card border border-1 border-dark d-flex flex-column align-items-center"
             style="width: 100%; max-width: 300px; margin:30px">
-            <img src="${producto.imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h4>${producto.titulo}</h4>
-                <p class="card-text">${producto.descripcion}</p>
-            </div>
-            <p class="card-text border border-secondary rounded p-2"><strong>${producto.precio}</strong></p>
-            <button class="btn btn-outline-success mt-auto mb-3" type="submit">Comprar</button>
-        </div>`;
-    });
+            <img src="${datos.imagen}" class="card-img-top" alt="...">
+            <div class="card-body ">
+                <h4>${datos.titulo}</h4>
+                <p class="card-text ">${datos.descripcion}</p>
 
-    contenedor.innerHTML = contenido;
+            </div>
+                <p class="card-text border border-secondary rounded p-2"><strong>${datos.precio}</strong></p>
+                <button class="btn btn-outline-success mt-auto mb-3" type="submit">Comprar</button>
+        </div>
+        `
+    })
+    contenedor.innerHTML = productos
 }
